150df7203811c98eb9ac260b16ac5e4ec4e7c0ca
Jonathan Simcoe <jonathan@simko.io>
2014-07-10 15:35:16 -0700

    fixes for links

--- workspace/assets/js/application.js
+++ workspace/assets/js/application.js
@@ -9689,24 +9689,24 @@ if(!jQuery)throw new Error("Bootstrap requires jQuery");+function(a){"use strict
   };
 // Works with either jQuery or Zepto
 })( window.jQuery || window.Zepto );
-;  $(document).ready(function () {
+;$(document).ready(function () {
 
-    $('.navbar-collapse').on('show.bs.collapse', function() {
-      $(".dropdown-normal").addClass("open");
-    });
-
-    $('.navbar-collapse').on('hide.bs.collapse', function() {
-      $(".dropdown-normal").removeClass("open");
-    });
-
-    $('.navbar-nav li.dropdown').mouseover(function() {
-      $(this).addClass("open");
-    });
-
-    $('.navbar-nav li.dropdown').mouseout(function() {
-      $(this).removeClass("open");
-    });
-
-    $('#content').fitVids();
-
+  $('.navbar-collapse').on('show.bs.collapse', function() {
+    $(".dropdown-normal").addClass("open");
   });
+
+  $('.navbar-collapse').on('hide.bs.collapse', function() {
+    $(".dropdown-normal").removeClass("open");
+  });
+
+  $('.navbar-nav li.dropdown').mouseover(function() {
+    $(this).addClass("open");
+  });
+
+  $('.navbar-nav li.dropdown').mouseout(function() {
+    $(this).removeClass("open");
+  });
+
+  $('#content').fitVids();
+
+});
--- workspace/assets/js/main.js
+++ workspace/assets/js/main.js
@@ -1,21 +1,21 @@
-  $(document).ready(function () {
+$(document).ready(function () {
 
-    $('.navbar-collapse').on('show.bs.collapse', function() {
-      $(".dropdown-normal").addClass("open");
-    });
-
-    $('.navbar-collapse').on('hide.bs.collapse', function() {
-      $(".dropdown-normal").removeClass("open");
-    });
-
-    $('.navbar-nav li.dropdown').mouseover(function() {
-      $(this).addClass("open");
-    });
-
-    $('.navbar-nav li.dropdown').mouseout(function() {
-      $(this).removeClass("open");
-    });
-
-    $('#content').fitVids();
-
+  $('.navbar-collapse').on('show.bs.collapse', function() {
+    $(".dropdown-normal").addClass("open");
   });
+
+  $('.navbar-collapse').on('hide.bs.collapse', function() {
+    $(".dropdown-normal").removeClass("open");
+  });
+
+  $('.navbar-nav li.dropdown').mouseover(function() {
+    $(this).addClass("open");
+  });
+
+  $('.navbar-nav li.dropdown').mouseout(function() {
+    $(this).removeClass("open");
+  });
+
+  $('#content').fitVids();
+
+});
